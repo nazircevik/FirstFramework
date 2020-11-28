9757b4cddc2004f391ef82ed6bead87a9160d7bf
Nazir Çevik <nzr.cevik@hotmail.com>
2020-11-29 01:13:43 +0300

    first busines

---
+++ FirstFramework/FirstFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using FirstFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFramework.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
